6dca35a4b9c86de0e814aec193973f56b5614776
TylerRKnightly <TylerRKnightly@gmail.com>
2025-04-24 10:13:23 -0400

    feat:setting up deploy configs

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import productRoutes from './routes/productRoutes';
 import authRoutes from './routes/authRoutes';
 import orderRoutes from './routes/orderRoutes';
+import path from 'path';
 
 const app = express();
 
@@ -12,5 +13,13 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 app.use('/api/auth', authRoutes);
 app.use('api/orders', orderRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
+    );
+  }
 
 export default app;
