df3ec8e12fed31f60947805f36ca037ff9184106
TylerRKnightly <TylerRKnightly@gmail.com>
2025-04-24 10:17:42 -0400

    fix: removed dupe code

--- server/src/app.ts
+++ server/src/app.ts
@@ -33,12 +33,4 @@ if (process.env.NODE_ENV === 'production') {
     );
   }
 
-if (process.env.NODE_ENV === 'production') {
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, '../client/build')));
-    app.get('*', (req, res) =>
-      res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
-    );
-  }
-
 export default app;
