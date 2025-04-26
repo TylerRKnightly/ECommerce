934e3a10b7a1adcd8c70714fa1d8d23c344c12df
VenBenvolio <vellencia.knightly@gmail.com>
2025-04-25 21:56:06 -0400

    feat: set up redux store and basic cart reducers

--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -5,21 +5,33 @@ import ProductCard from '../components/ProductCard';
 import ProductsFilter from '../components/ProductsFilter';
 import { getBreadcrumb } from '../utils/breadcrumb';
 import { unslugify } from '../utils/format';
+import { useDispatch } from 'react-redux';
+import { ProductData } from '../types/product';
+import { addToCart } from '../store/cartSlice';
+import { Link } from 'react-router-dom';
 
 const Products = () => {
     const { categoryName } = useParams();
-    const bcSegments = ['Products']
+    const dispatch = useDispatch();
+    const bcSegments = ['Products'];
 
     const getProducts = () => {
         const snakeCatName = categoryName?.replace(/-/g, '_');
         return snakeCatName ? mockProducts.filter(p => p.category.name === snakeCatName) : mockProducts;
     }
 
+    const handleAddToCart = (product: ProductData) => {
+        dispatch(addToCart(product));
+    };
+
     return (
         <div className='row mx-auto' style={{ maxWidth: '1200px' }}>
             <ol className='breadcrumb mx-3'>
+                <li className='breadcrumb-item' key={'home'}>
+                    <Link to='/' className='text-decoration-none text-muted'>Home</Link>
+                </li>
                 {getBreadcrumb(bcSegments)}
-                <li className="breadcrumb-item active" aria-current="page">
+                <li className="breadcrumb-item active" aria-current="page" key={categoryName}>
                     {(categoryName && unslugify(categoryName)) || 'All'}
                 </li>
             </ol>
@@ -30,7 +42,7 @@ const Products = () => {
                 <div className="row">
                     {getProducts().map((p) => (
                         <div className='col-auto p-0' key={p._id}>
-                            <ProductCard product={p} />
+                            <ProductCard product={p} onAddToCart={handleAddToCart} />
                         </div>
                     ))}
                 </div>
