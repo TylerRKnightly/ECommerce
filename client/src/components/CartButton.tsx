0bac44a05215d1957de521ead8e52e4ed6159658
VenBenvolio <vellencia.knightly@gmail.com>
2025-04-28 13:32:52 -0400

    feat: implement increase/reduce/remove item management

--- client/src/components/CartButton.tsx
+++ client/src/components/CartButton.tsx
@@ -1,15 +1,17 @@
 import React, { useRef } from "react";
 import { ReactComponent as CartIcon } from '../assets/cart-shopping-solid.svg'
+import { ReactComponent as DeleteIcon } from '../assets/trash-can-regular.svg'
 import { Offcanvas } from 'bootstrap';
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store/store";
 import { Link } from "react-router-dom";
 import { formatCurrency } from "../utils/format";
+import { updateQuantity, removeFromCart } from '../store/cartSlice';
 
 const CartButton = () => {
     const cartItems = useSelector((state: RootState) => state.cart.cartItems);
-
     const cartRef = useRef<HTMLDivElement>(null);
+    const dispatch = useDispatch();
 
 
     const openCart = () => {
@@ -31,20 +33,44 @@ const CartButton = () => {
 
         <div ref={cartRef} className='offcanvas offcanvas-end' tabIndex={-1} aria-labelledby='cartSidebarLabel'>
             <div className="offcanvas-header">
-                <h5>Your Cart {cartItems && `(${cartItems.length} Items)`}</h5>
+                <h5>Your Cart {cartItems.length > 0 && `(${cartItems.reduce((prev, curr) => prev + curr.quantity, 0)} Items)`}</h5>
                 <button className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <hr />
             <div className="offcanvas-body">
                 {cartItems.length === 0 ? (<p>Your cart is empty</p>) : (
                     <>
-                        <ul>{
+                        <ul className="list-group">{
                             cartItems.map(i => (
                                 <li className="list-group-item" key={i._id}>
-                                    {i.name}
-                                    <div className="d-flex justify-content-between text-muted">
-                                        x{i.quantity}
-                                        <span>{formatCurrency(i.price, true)}</span>
+                                    <div className="d-flex justify-content-between">
+                                        {i.name}
+                                        <span className="text-muted">{formatCurrency(i.price, true)}</span>
+                                    </div>
+                                    <div className="d-flex justify-content-between">
+                                        <div className="d-flex align-items-center">
+                                            <button
+                                                className='btn btn-sm'
+                                                onClick={(e) => {
+                                                    setTimeout(() => {
+                                                        (e.target as HTMLButtonElement).blur();
+                                                    }, 0);
+                                                    dispatch(updateQuantity({ id: i._id, quantity: i.quantity - 1 }));
+                                                }}
+                                                disabled={i.quantity <= 1}
+                                            >-</button>
+                                            <span className="mx-1"><small>{i.quantity}</small></span>
+                                            <button
+                                                className='btn btn-sm'
+                                                onClick={() => dispatch(updateQuantity({ id: i._id, quantity: i.quantity + 1 }))}
+                                            >+</button>
+                                        </div>
+                                        <div>
+                                            <button
+                                                className="btn"
+                                                onClick={() => dispatch(removeFromCart(i._id))}
+                                            ><DeleteIcon style={{ color: 'crimson', width: '1rem' }} /></button>
+                                        </div>
                                     </div>
                                 </li>
                             ))
@@ -55,8 +81,7 @@ const CartButton = () => {
                             <span>{formatCurrency(cartItems.reduce((prev, curr) => prev + curr.price * curr.quantity, 0), true)}</span>
                         </div>
                         <div className="mt-3">
-                            <Link to="/" className="btn w-100" onClick={closeCart}
-                            >Checkout</Link>
+                            <Link to="/" className="btn w-100" onClick={closeCart}>Checkout</Link>
                         </div>
                     </>
                 )}
